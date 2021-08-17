c4525fe31d6d0eb71a768670b636071eeff0e7f6
Hendra Anggrian <hendraanggrian@gmail.com>
2021-08-18 03:43:31 +0700

    Update Kotlin and Gradle

--- buildSrc/src/dependencies.kt
+++ buildSrc/src/dependencies.kt
@@ -1,9 +1,9 @@
 internal typealias Plugins = org.gradle.plugin.use.PluginDependenciesSpec
 internal typealias Dependencies = org.gradle.api.artifacts.dsl.DependencyHandler
 
-const val VERSION_KOTLIN = "1.5.10"
-const val VERSION_COROUTINES = "1.4.3"
-val Dependencies.dokka get() = "org.jetbrains.dokka:dokka-gradle-plugin:1.4.32"
+const val VERSION_KOTLIN = "1.5.21"
+const val VERSION_COROUTINES = "1.5.1"
+val Dependencies.dokka get() = "org.jetbrains.dokka:dokka-gradle-plugin:1.5.0"
 val Plugins.dokka get() = id("org.jetbrains.dokka")
 fun Dependencies.kotlinx(module: String, version: String? = null) =
     "org.jetbrains.kotlinx:kotlinx-$module${version?.let { ":$it" }.orEmpty()}"
@@ -14,6 +14,6 @@ val Plugins.`git-publish` get() = id("org.ajoberstar.git-publish")
 const val VERSION_KOTLINPOET = "1.8.0"
 fun Dependencies.squareup(module: String, version: String) = "com.squareup:$module:$version"
 
-const val VERSION_TRUTH = "1.0.1"
+const val VERSION_TRUTH = "1.1.3"
 fun Dependencies.google(repo: String? = null, module: String, version: String) =
     "com.google${repo?.let { ".$it" }.orEmpty()}:$module:$version"
