9739a9441002ece880851c1681bd0d4d0cf9420c
Hendra Anggrian <hendraanggrian@gmail.com>
2022-06-20 16:42:45 +0700

    Rearrange badges

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,7 +1,4 @@
-
-import com.diffplug.gradle.spotless.SpotlessExtension
-import com.vanniktech.maven.publish.MavenPublishBaseExtension
-import org.jetbrains.dokka.gradle.DokkaTask
+import org.jetbrains.kotlin.gradle.dsl.KotlinProjectExtension
 
 buildscript {
     repositories {
@@ -29,42 +26,8 @@ allprojects {
 
 subprojects {
     afterEvaluate {
-        extensions.find<org.jetbrains.kotlin.gradle.dsl.KotlinProjectExtension>()?.jvmToolchain {
+        extensions.find<KotlinProjectExtension>()?.jvmToolchain {
             (this as JavaToolchainSpec).languageVersion.set(JavaLanguageVersion.of(sdk.versions.jdk.get()))
-        }
-        tasks.find<DokkaTask>("dokkaHtml") {
-            outputDirectory.set(buildDir.resolve("dokka/dokka"))
-        }
-        extensions.find<SpotlessExtension>()?.kotlin {
-            ktlint()
-        }
-        extensions.find<MavenPublishBaseExtension> {
-            publishToMavenCentral(com.vanniktech.maven.publish.SonatypeHost.S01)
-            signAllPublications()
-            pom {
-                name.set(project.name)
-                description.set(RELEASE_DESCRIPTION)
-                url.set(RELEASE_URL)
-                licenses {
-                    license {
-                        name.set("The Apache License, Version 2.0")
-                        url.set("http://www.apache.org/licenses/LICENSE-2.0.txt")
-                        distribution.set("http://www.apache.org/licenses/LICENSE-2.0.txt")
-                    }
-                }
-                scm {
-                    connection.set("scm:git:https://github.com/$DEVELOPER_ID/$RELEASE_ARTIFACT.git")
-                    developerConnection.set("scm:git:ssh://git@github.com/$DEVELOPER_ID/$RELEASE_ARTIFACT.git")
-                    url.set(RELEASE_URL)
-                }
-                developers {
-                    developer {
-                        id.set(DEVELOPER_ID)
-                        name.set(DEVELOPER_NAME)
-                        url.set(DEVELOPER_URL)
-                    }
-                }
-            }
         }
     }
 }
--- kotlinpoet-dsl/build.gradle.kts
+++ kotlinpoet-dsl/build.gradle.kts
@@ -1,5 +1,6 @@
 import com.vanniktech.maven.publish.JavadocJar
 import com.vanniktech.maven.publish.KotlinJvm
+import com.vanniktech.maven.publish.SonatypeHost.*
 
 plugins {
     kotlin("jvm")
@@ -9,7 +10,39 @@ plugins {
     id("com.vanniktech.maven.publish.base")
 }
 
-mavenPublishing.configure(KotlinJvm(JavadocJar.Dokka("dokkaJavadoc")))
+kover.generateReportOnCheck = false
+
+spotless.kotlin { ktlint() }
+
+mavenPublishing {
+    publishToMavenCentral(S01)
+    signAllPublications()
+    pom {
+        name.set(project.name)
+        description.set(RELEASE_DESCRIPTION)
+        url.set(RELEASE_URL)
+        licenses {
+            license {
+                name.set("The Apache License, Version 2.0")
+                url.set("http://www.apache.org/licenses/LICENSE-2.0.txt")
+                distribution.set("http://www.apache.org/licenses/LICENSE-2.0.txt")
+            }
+        }
+        scm {
+            connection.set("scm:git:https://github.com/$DEVELOPER_ID/$RELEASE_ARTIFACT.git")
+            developerConnection.set("scm:git:ssh://git@github.com/$DEVELOPER_ID/$RELEASE_ARTIFACT.git")
+            url.set(RELEASE_URL)
+        }
+        developers {
+            developer {
+                id.set(DEVELOPER_ID)
+                name.set(DEVELOPER_NAME)
+                url.set(DEVELOPER_URL)
+            }
+        }
+    }
+    configure(KotlinJvm(JavadocJar.Dokka("dokkaJavadoc")))
+}
 
 dependencies {
     api(libs.kotlinpoet)
@@ -17,6 +50,11 @@ dependencies {
     testImplementation(testLibs.truth)
 }
 
-tasks.compileKotlin {
-    kotlinOptions.freeCompilerArgs += "-opt-in=kotlin.RequiresOptIn"
+tasks {
+    compileKotlin {
+        kotlinOptions.freeCompilerArgs += "-opt-in=kotlin.RequiresOptIn"
+    }
+    dokkaHtml {
+        outputDirectory.set(buildDir.resolve("dokka/dokka"))
+    }
 }
